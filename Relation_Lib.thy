cb8b778fbd3316fb4fad6c654410e3abac4e046d
simondfoster <simondf@gmail.com>
2021-02-18 11:00:34 +0000

    Added a few extra library theories

--- Relation_Lib.thy
+++ Relation_Lib.thy
@@ -1,5 +1,10 @@
 section \<open> Meta-theory for Relation Library \<close>
 
 theory Relation_Lib
-  imports FSet_Extra Relation_Extra Partial_Fun Finite_Fun Total_Fun List_Extra
-begin end
+  imports Countable_Set_Extra Positive FSet_Extra Relation_Extra Partial_Fun Finite_Fun Total_Fun List_Extra
+begin 
+
+text \<open> This theory marks the boundary between reusable library utilities and the creation of the
+  Z notation. We avoid overriding any HOL syntax up until this point. \<close>
+
+end
