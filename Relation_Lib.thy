e9a4f97380519b33acc2e77872d9dbc72cfc9a30
Simon Foster <simon.foster@york.ac.uk>
2022-02-01 12:35:10 +0000

    Added polymorphic notation for the refinement operator

--- Relation_Lib.thy
+++ Relation_Lib.thy
@@ -31,4 +31,36 @@ notation Fpow ("\<bbbF>")
 
 end
 
+class refine =
+  fixes ref_by :: "'a \<Rightarrow> 'a \<Rightarrow> bool" (infix "\<sqsubseteq>" 50)
+  and sref_by :: "'a \<Rightarrow> 'a \<Rightarrow> bool"  (infix "\<sqsubset>" 50)
+  assumes ref_by_order: "class.preorder (\<sqsubseteq>) (\<sqsubset>)"
+
+interpretation ref_preorder: preorder "(\<sqsubseteq>)" "(\<sqsubset>)"
+  by (fact ref_by_order)
+
+lemma ref_by_trans [trans]: "\<lbrakk> P \<sqsubseteq> Q; Q \<sqsubseteq> R \<rbrakk> \<Longrightarrow> P \<sqsubseteq> R"
+  using ref_preorder.dual_order.trans by auto
+
+instantiation "bool" :: refine
+begin
+
+definition "ref_by_bool P Q = (Q \<longrightarrow> P)"
+definition "sref_by_bool P Q = (\<not> Q \<and> P)"
+
+instance by (intro_classes, unfold_locales, auto simp add: ref_by_bool_def sref_by_bool_def)
+
+end
+
+instantiation "fun" :: (type, refine) refine
+begin
+
+definition ref_by_fun :: "('a \<Rightarrow> 'b) \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> bool" where "ref_by_fun f g = (\<forall> x. f(x) \<sqsubseteq> g(x))"
+definition sref_by_fun :: "('a \<Rightarrow> 'b) \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> bool" where "sref_by_fun f g = (f \<sqsubseteq> g \<and> \<not> (g \<sqsubseteq> f))"
+
+instance 
+  by (intro_classes, unfold_locales)
+     (auto simp add: ref_by_fun_def sref_by_fun_def ref_preorder.less_le_not_le intro: ref_preorder.order.trans)
+end
+
 end
