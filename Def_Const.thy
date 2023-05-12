5e86b10cdfd7607db7b1c3a19fa58e7f7dd1bd9c
Simon Foster <simon.foster@york.ac.uk>
2023-05-12 13:57:27 +0100

    Made public the functionality of def_const and def_consts

--- Def_Const.thy
+++ Def_Const.thy
@@ -9,18 +9,22 @@ text \<open> Add a simple command to define previously declared polymorphic cons
   particularly useful for handling given sets in Z. \<close>
 
 ML \<open>
-let fun def_const (n, v) thy = 
+
+structure Def_Const =
+struct
+
+fun def_const (n, v) thy = 
   let val Const (pn, typ) = Proof_Context.read_const {proper = false, strict = false} (Named_Target.theory_init thy) n
       val ctx = Overloading.overloading [(n, (pn, dummyT), false)] thy
       val pt = Syntax.check_term ctx (Type.constraint typ (Syntax.parse_term ctx v))
   in (Local_Theory.exit_global o snd o Local_Theory.define (((Binding.name n), NoSyn), ((Binding.name (n ^ "_def"), [Attrib.check_src @{context} (Token.make_src ("code", Position.none) [])]), pt))) ctx 
   end 
-  val def_consts = fold def_const
-in
+val def_consts = fold def_const
+
+end;
 
 Outer_Syntax.command @{command_keyword def_consts} "define a declared constant"
-    (Scan.repeat1 ((Parse.name --| @{keyword "="}) -- Parse.term) >> (Toplevel.theory o def_consts))
-  end
+    (Scan.repeat1 ((Parse.name --| @{keyword "="}) -- Parse.term) >> (Toplevel.theory o Def_Const.def_consts));
 \<close>
 
 end
